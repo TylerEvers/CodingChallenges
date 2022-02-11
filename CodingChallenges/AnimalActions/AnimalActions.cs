7c37cd65e9457b0ad5101ff10c1d1691fb4c216a
Tyler Evers <TEves2015@gmail.com>
2022-02-10 21:30:40 -0600

    Add project files.

---
+++ CodingChallenges/AnimalActions/AnimalActions.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace AnimalActionsChallenge
+{
+    internal static class AnimalActions //Action Class
+    {
+        public static void Eat(Animal animal)
+        {
+            animal.OutputEat();
+        }
+
+        public static void Swim(Animal animal)
+        {
+            animal.OutputSwim();
+        }
+
+        public static void Fly(Animal animal)
+        {
+            animal.OutputFly();
+        }
+
+        public static void GetAnimalSound(Animal animal)
+        {
+            animal.OutputSound();
+        }
+    }
+
+    class Animal //Base Animal Class
+    {
+        public virtual void OutputEat()
+        {
+            Console.WriteLine("All animals eat, I think");
+        }
+
+        public virtual void OutputSwim()
+        {
+            Console.WriteLine("This animal doesn't swim");
+        }
+
+        public virtual void OutputFly()
+        {
+            Console.WriteLine("This animal doesn't fly");
+        }
+
+        public virtual void OutputSound()
+        {
+            Console.WriteLine("This animal is silent");
+        }
+    }
+
+    class Parrot : Animal
+    {
+        public override void OutputFly()
+        {
+            Console.WriteLine("Flaps wings and takes off from the canopy");
+        }
+
+        public override void OutputSound()
+        {
+            Console.WriteLine("SQUAWK");
+        }
+    }
+
+    class Penguin : Animal
+    {
+        public override void OutputSwim()
+        {
+            Console.WriteLine("Dives into the icy waters");
+        }
+
+        public override void OutputSound()
+        {
+            Console.WriteLine("HONK");
+        }
+    }
+
+    class Pterodactyl : Animal
+    {
+        public override void OutputFly()
+        {
+            Console.WriteLine("Flaps wings and glides through the sky");
+        }
+
+        public override void OutputSound()
+        {
+            Console.WriteLine("KWAAAAA");
+        }
+    }
+
+    class Duck : Animal
+    {
+        public override void OutputSwim()
+        {
+            Console.WriteLine("Floats around on the pond");
+        }
+
+        public override void OutputSound()
+        {
+            Console.WriteLine("QUACK");
+        }
+    }
+}
+
---
+++ CodingChallenges/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using AnimalActionsChallenge;
+
+namespace CodingChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Animal Actions Challenge
+            Parrot parrot = new();
+        
+            Console.WriteLine("Parrot");
+            parrot.OutputEat();
+            parrot.OutputFly();
+            parrot.OutputSwim();
+            parrot.OutputSound();
+        
+            Penguin penguin = new();
+        
+            Console.WriteLine("Penguin");
+            penguin.OutputEat();
+            penguin.OutputFly();
+            penguin.OutputSwim();
+            penguin.OutputSound();
+        
+            Pterodactyl pterodactyl = new();
+        
+            Console.WriteLine("Pterodactyl");
+            pterodactyl.OutputEat();
+            pterodactyl.OutputFly();
+            pterodactyl.OutputSwim();
+            pterodactyl.OutputSound();
+        
+            Duck duck = new();
+        
+            Console.WriteLine("Duck");
+            duck.OutputEat();
+            duck.OutputFly();
+            duck.OutputSwim();
+            duck.OutputSound();
+        }
+    }
+}
