fd1150a6d2296b6fc7fe11c9f281c5828088e776
Tyler Evers <TEves2015@gmail.com>
2022-12-14 13:54:32 -0600

    Restructured project for future additions, added basic logging, added the clock angles project - see readme for details.

---
+++ CodingChallenges/AnimalActions/Program.cs
@@ -0,0 +1,48 @@
+﻿using AnimalActionsChallenge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenges.AnimalActions
+{
+    internal class Program
+    {
+        internal static void AnimalActions()
+        {
+            //Animal Actions Challenge
+            Parrot parrot = new();
+
+            Console.WriteLine("Parrot");
+            parrot.OutputEat();
+            parrot.OutputFly();
+            parrot.OutputSwim();
+            parrot.OutputSound();
+
+            Penguin penguin = new();
+
+            Console.WriteLine("Penguin");
+            penguin.OutputEat();
+            penguin.OutputFly();
+            penguin.OutputSwim();
+            penguin.OutputSound();
+
+            Pterodactyl pterodactyl = new();
+
+            Console.WriteLine("Pterodactyl");
+            pterodactyl.OutputEat();
+            pterodactyl.OutputFly();
+            pterodactyl.OutputSwim();
+            pterodactyl.OutputSound();
+
+            Duck duck = new();
+
+            Console.WriteLine("Duck");
+            duck.OutputEat();
+            duck.OutputFly();
+            duck.OutputSwim();
+            duck.OutputSound();
+        }
+    }
+}
--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -7,38 +7,15 @@ namespace CodingChallenges
     {
         static void Main(string[] args)
         {
-            //Animal Actions Challenge
-            Parrot parrot = new();
-        
-            Console.WriteLine("Parrot");
-            parrot.OutputEat();
-            parrot.OutputFly();
-            parrot.OutputSwim();
-            parrot.OutputSound();
-        
-            Penguin penguin = new();
-        
-            Console.WriteLine("Penguin");
-            penguin.OutputEat();
-            penguin.OutputFly();
-            penguin.OutputSwim();
-            penguin.OutputSound();
-        
-            Pterodactyl pterodactyl = new();
-        
-            Console.WriteLine("Pterodactyl");
-            pterodactyl.OutputEat();
-            pterodactyl.OutputFly();
-            pterodactyl.OutputSwim();
-            pterodactyl.OutputSound();
-        
-            Duck duck = new();
-        
-            Console.WriteLine("Duck");
-            duck.OutputEat();
-            duck.OutputFly();
-            duck.OutputSwim();
-            duck.OutputSound();
+            //Animal Actions
+            Console.WriteLine("Beginning Animal Actions: " + DateTime.Now);
+            AnimalActions.Program.AnimalActions();
+            Console.WriteLine("Ending Animal Actions: " + DateTime.Now + "\x0A");
+
+            //Clock Angles
+            Console.WriteLine("Beginning Clock Angles: " + DateTime.Now);
+            ClockAngles.Program.ClockAngles();
+            Console.WriteLine("Beginning Clock Angles: " + DateTime.Now + "\x0A");
         }
     }
 }
