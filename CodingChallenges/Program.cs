b937ba7066a04472caff30c84fed6964a0ab113b
Tyler Evers <TEves2015@gmail.com>
2024-01-05 01:11:12 -0600

    Updated .NET version, implemented Binary Search function, cleaned-up unecessary usings, created BogusGenerators for test data to avoid requiring user entry for each example

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using AnimalActionsChallenge;
 
 namespace CodingChallenges
 {
@@ -11,11 +10,16 @@ namespace CodingChallenges
             Console.WriteLine("Beginning Animal Actions: " + DateTime.Now);
             AnimalActions.Program.AnimalActions();
             Console.WriteLine("Ending Animal Actions: " + DateTime.Now + "\x0A");
-
+            
             //Clock Angles
             Console.WriteLine("Beginning Clock Angles: " + DateTime.Now);
             ClockAngles.Program.ClockAngles();
             Console.WriteLine("Beginning Clock Angles: " + DateTime.Now + "\x0A");
+
+            //Binary Search
+            Console.WriteLine("Beginning Binary Search: " + DateTime.Now);
+            BinarySearch.Program.BinarySearch();
+            Console.WriteLine("Beginning Binary Search: " + DateTime.Now + "\x0A");
         }
     }
 }
